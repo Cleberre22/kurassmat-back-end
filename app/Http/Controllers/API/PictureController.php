b49a340ec1726bf21c37d8aa92608f9692ac5bc9
Cleberre22 <c.leberre@hotmail.fr>
2022-11-14 10:52:06 +0100

    maj

--- app/Http/Controllers/API/PictureController.php
+++ app/Http/Controllers/API/PictureController.php
@@ -6,6 +6,7 @@ use App\Models\Picture;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use App\Http\Controllers\Controller;
+use Intervention\Image\Facades\Image;
 
 class PictureController extends Controller
 {
@@ -16,17 +17,17 @@ class PictureController extends Controller
      */
     public function index()
     {
-               // On récupère tous les photos
-               $pictures = Picture::orderBy('created_at')->get();
+        // On récupère tous les photos
+        $pictures = Picture::orderBy('created_at')->get();
 
-               // On retourne les informations des utilisateurs en JSON
-               return response()->json([
-                   'status' => 'Success',
-                   'data' => $pictures,
-               ]);
+        // On retourne les informations des utilisateurs en JSON
+        return response()->json([
+            'status' => 'Success',
+            'data' => $pictures,
+        ]);
     }
 
-       /**
+    /**
      * Display a listing of the resource.
      *
      * @return \Illuminate\Http\Response
@@ -58,47 +59,38 @@ class PictureController extends Controller
      */
     public function store(Request $request)
     {
-        $request->validate([
-            'urlPicture' => 'required|image|mimes:jpeg,png,jpg,gif,svg',
+        $this->validate($request, [
+            'urlPicture' => 'required|image|mimes:jpg,jpeg,png,gif,svg|10000',
             'namePicture' => 'required|max:100',
-            'childs_id' => 'required'
+            'childs_id'
         ]);
+        $image = $request->file('urlPicture');
+        $input['urlPicture'] = time() . '.' . $image->getClientOriginalExtension();
 
-        $filename = "";
-        if ($request->hasFile('urlPicture')) {
-            // On récupère le nom du fichier avec son extension, résultat $filenameWithExt : "jeanmiche.jpg"
-            $filenameWithExt = $request->file('urlPicture')->getClientOriginalName();
-            $filenameWithoutExt = pathinfo($filenameWithExt, PATHINFO_FILENAME);
-            // On récupère l'extension du fichier, résultat $extension : ".jpg"
-            $extension = $request->file('urlPicture')->getClientOriginalExtension();
-            // On créer un nouveau fichier avec le nom + une date + l'extension, résultat $fileNameToStore :"jeanmiche_20220422.jpg"
-            $filename = $filenameWithoutExt . '_' . time() . '.' . $extension;
-
-
-            $filename = $request->file('urlPicture');
-            $filename = Picture::make($filename->path());
-            $filename->resize(100, 100, function ($constraint) {
-                $constraint->aspectRatio();
-            })->save($request.'public/uploads'.$filename);
-
-
-
-            // On enregistre le fichier à la racine /storage/app/public/uploads, ici la méthode storeAs défini déjà le chemin /storage/app
-        //     $path = $request->file('urlPicture')->storeAs('public/uploads', $filename);
-        // } else {
-        //     $filename = Null;
-        // }
+        $destinationPath = public_path('/thumbnail');
+        $imgFile = Image::make($image->getRealPath());
+        $imgFile->resize(250, 250, function ($constraint) {
+            $constraint->aspectRatio();
+        })->save($destinationPath . '/' . $input['urlPicture']);
+        $destinationPath = public_path('/uploads');
+        $image->move($destinationPath, $input['urlPicture']);
+        // return back()
+        // 	->with('success','Image has successfully uploaded.')
+        // 	->with('fileName',$input['urlPicture']);
+        // dd($image);
 
         // On crée une nouvelle photo
-        $pictures = Picture::create([
-            
-            'urlPicture' => $filename,
+        $image = Picture::create([
+            'urlPicture' => $input['urlPicture'],
             'namePicture' => $request->namePicture,
             'childs_id' => $request->childs_id,
         ]);
+
         // On retourne les informations du nouveau message de contact en JSON
-        return response()->json($pictures, 201);
+        return response()->json($image, 201);
     }
+
+
 
     /**
      * Display the specified resource.
@@ -108,8 +100,8 @@ class PictureController extends Controller
      */
     public function show(Picture $picture)
     {
-         // On retourne les informations d'un type de fichier en JSON
-         return response()->json([
+        // On retourne les informations d'un type de fichier en JSON
+        return response()->json([
             'status' => 'Success',
             'data' => $picture,
         ]);
@@ -124,11 +116,11 @@ class PictureController extends Controller
      */
     public function destroy(Picture $picture)
     {
-          // On supprime la photo
-       $picture->delete();
-       // On retourne la réponse JSON
-       return response()->json([
-           'status' => 'Supprimé avec succès'
-       ]);
+        // On supprime la photo
+        $picture->delete();
+        // On retourne la réponse JSON
+        return response()->json([
+            'status' => 'Supprimé avec succès'
+        ]);
     }
 }
