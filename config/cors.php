541f6be3772da72e4fbd01a1b4a782bbe02db27c
Cleberre22 <c.leberre@hotmail.fr>
2023-01-11 15:48:43 +0100

    [ ADD ] Security Cors, Csrf, Middleware

--- config/cors.php
+++ config/cors.php
@@ -15,7 +15,11 @@ return [
     |
     */
 
-    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+    'paths' => ['api/login',
+                'api/logout',
+                'api/registerAssmat',
+                'api/registerEmployer',
+                'sanctum/csrf-cookie'],
 
     'allowed_methods' => ['*'],
 
